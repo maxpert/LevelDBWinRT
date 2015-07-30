137bab88893828ff6defd112b4cc5f56ebc711bf
Zohaib Sibte Hassan <zohaibs@microsoft.com>
2015-07-29 19:47:22 -0700

    Adding parallel writes test

--- UnitTest.Shared/UnitTest.cs
+++ UnitTest.Shared/UnitTest.cs
@@ -1,6 +1,8 @@
 ﻿namespace UnitTest.Universal
 {
     using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
     using LevelDBWinRT;
     using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 
@@ -68,6 +70,44 @@
                 Assert.AreEqual(j, BitConverter.ToInt32(readSlice.ToByteArray(), 0));
                 Assert.IsTrue(this.levelDB.Delete(writeOptions, key));
             }
+        }
+
+        [TestMethod]
+        public void TestParallelWrites()
+        {
+            const int totalInserts = 100;
+            var tasks = new List<Task<int>>();
+            for (var i = 0; i < 10; i++)
+            {
+               tasks.Add(this.ParallelWriter(totalInserts)); 
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            foreach (var task in tasks)
+            {
+                Assert.AreEqual(totalInserts, task.Result);
+            }
+        }
+
+        private async Task<int> ParallelWriter(int totalInserts)
+        {
+            WriteOptions writeOptions = new WriteOptions();
+            int counter = 0;
+            for (var i = 0; i < totalInserts; i++)
+            {
+                if (this.levelDB.Put(
+                    writeOptions, 
+                    Slice.FromByteArray(BitConverter.GetBytes(i)),
+                    Slice.FromString(DateTime.Now.ToString())))
+                {
+                    counter++;
+                }
+
+                await Task.Delay(10).ConfigureAwait(false);
+            }
+
+            return counter;
         }
     }
 }
