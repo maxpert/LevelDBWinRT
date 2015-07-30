8b10b0df18016f0741b08195fe5320e67faaed5e
Zohaib Sibte Hassan <zohaibs@microsoft.com>
2015-07-29 19:36:07 -0700

    Basic tests

--- UnitTest.Shared/UnitTest.cs
+++ UnitTest.Shared/UnitTest.cs
@@ -1,5 +1,6 @@
 ﻿namespace UnitTest.Universal
 {
+    using System;
     using LevelDBWinRT;
     using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 
@@ -29,7 +30,44 @@
         [TestMethod]
         public void TestPut()
         {
-            this.levelDB.Put(new WriteOptions(), Slice.FromString("foo"), Slice.FromString("bar"));
+            Assert.IsTrue(this.levelDB.Put(new WriteOptions(), Slice.FromString("foo"), Slice.FromString("bar")));
+            Assert.AreEqual(
+                "bar",
+                this.levelDB.Get(new ReadOptions(), Slice.FromString("foo")).AsString());
+        }
+
+        [TestMethod]
+        public void TestDelete()
+        {
+            Assert.IsTrue(this.levelDB.Put(new WriteOptions(), Slice.FromString("foo"), Slice.FromString("bar")));
+            Assert.IsTrue(this.levelDB.Delete(new WriteOptions(), Slice.FromString("foo")));
+            Assert.AreEqual(
+                null,
+                this.levelDB.Get(new ReadOptions(), Slice.FromString("foo")));
+        }
+
+
+        [TestMethod]
+        public void TestBatchWrite()
+        {
+            WriteBatch writeBatch = new WriteBatch();
+            ReadOptions readOptions = new ReadOptions();
+            WriteOptions writeOptions = new WriteOptions();
+            for (var i = 0; i < 10; i++)
+            {
+                writeBatch.Put(Slice.FromString("Key"+i), Slice.FromByteArray(BitConverter.GetBytes(i)));
+            }
+
+            this.levelDB.Write(writeOptions, writeBatch);
+            
+            for (var j = 0; j < 10; j++)
+            {
+                Slice key = Slice.FromString("Key" + j);
+                Slice readSlice = this.levelDB.Get(readOptions, key);
+                Assert.IsNotNull(readSlice);
+                Assert.AreEqual(j, BitConverter.ToInt32(readSlice.ToByteArray(), 0));
+                Assert.IsTrue(this.levelDB.Delete(writeOptions, key));
+            }
         }
     }
 }
