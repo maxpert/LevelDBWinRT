165e7a9df721e7e3208aa2ce23da8266cc6babc8
Zohaib Sibte Hassan <zohaibs@microsoft.com>
2015-07-29 17:20:52 -0700

    Fixing compile issues and introducing UnitTest environment

---
+++ UnitTest.Universal/UnitTest.cs
@@ -0,0 +1,35 @@
+﻿namespace UnitTest.Universal
+{
+    using LevelDBWinRT;
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+    [TestClass]
+    public class BasicAPITests
+    {
+        private DB levelDB;
+
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.levelDB = new DB("foo.bar");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this.levelDB.Dispose();
+        }
+
+        [TestMethod]
+        public void TestPut()
+        {
+            this.levelDB.Put(new WriteOptions(), Slice.FromString("foo"), Slice.FromString("bar"));
+        }
+    }
+}
