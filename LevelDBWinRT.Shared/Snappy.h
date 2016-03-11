424f007683d5b6a5468e39bdefe2a88a11cab79f
Zohaib <zohaibsh@gmail.com>
2016-03-10 21:50:17 -0800

    Removing usless classes

--- LevelDBWinRT.Shared/Snappy.h
+++ LevelDBWinRT.Shared/Snappy.h
@@ -11,35 +11,25 @@ using namespace Platform::Runtime;
 
 namespace LevelDBWinRT {
 	namespace Snappy {
-		public ref class ArrayBuffer sealed {
-		private:
-			Array<uint8>^ buffer;
-		internal:
-			ArrayBuffer(char *buffer, size_t bufferLength) {
-				this->buffer = ref new Array<uint8>(bufferLength);
-				memcpy(this->buffer->Data, buffer, bufferLength);
-			}
-		public:
-			Array<uint8>^ GetBytes() {
-				return this->buffer;
-			}
-
-		};
-
 		public ref class Snappy sealed {
 		public:
-			static ArrayBuffer^ Compress(const Array<uint8>^ data) {
+			static Array<uint8>^ Compress(const Array<uint8>^ data) {
 				char *compressed_out = new char[snappy::MaxCompressedLength(data->Length)];
 				size_t compressed_len = 0;
 
 				snappy::RawCompress((const char *)data->Data, data->Length, compressed_out, &compressed_len);
-				auto ret = ref new ArrayBuffer(compressed_out, compressed_len);
-				delete[] compressed_out;
+				auto ret = ref new Array<uint8>(compressed_len);
+        if (ret == nullptr)
+        {
+          return nullptr;
+        }
 
+        memcpy(ret->Data, compressed_out, compressed_len);
+				delete[] compressed_out;
 				return ret;
 			}
 
-			static ArrayBuffer^ Uncompress(const Array<uint8> ^data) {
+			static Array<uint8>^ Uncompress(const Array<uint8> ^data) {
 				size_t uncompressed_length = 0;
 				if (!snappy::GetUncompressedLength((const char*)data->Data, data->Length, &uncompressed_length)) {
 					return nullptr;
@@ -51,9 +41,14 @@ namespace LevelDBWinRT {
 					return nullptr;
 				}
 
-				auto ret = ref new ArrayBuffer(uncompressed, uncompressed_length);
-				delete[] uncompressed;
+				auto ret = ref new Array<uint8>(uncompressed_length);
+        if (ret == nullptr)
+        {
+          return nullptr;
+        }
 
+        memcpy(ret->Data, uncompressed, uncompressed_length);
+				delete[] uncompressed;
 				return ret;
 			}
 		};
